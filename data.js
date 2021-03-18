995f58bfc54ef1bfb40f5d765469bedc40245976
Hamzagecer <etemmmete14@gmail.com>
2021-03-18 23:38:21 +0100

    first commit

--- data.js
+++ data.js
@@ -1,111 +1,110 @@
-const fishFarm = [ 
-    { 
+const fishFarm = [{
         fishType: "Salmon",
         price: 15.70,
-        entryDate: new Date(2021, 0, 1),    // 01.01.2021
+        entryDate: new Date(2021, 0, 1), // 01.01.2021
         durationInDays: 30,
         itemWeightInGrams: 145,
-        originCountry: "Norway", 
-        season: "Winter", 
+        originCountry: "Norway",
+        season: "Winter",
         stockVolumeInKg: 6500,
         saleLocations: ["ZH", "GE", "BE", "VD"],
     },
-    { 
+    {
         fishType: "Seatrout",
         price: 7.90,
-        entryDate: new Date(2021, 11, 12),    // 12.12.2021
+        entryDate: new Date(2021, 11, 12), // 12.12.2021
         durationInDays: 20,
         itemWeightInGrams: 460,
-        originCountry: "Japan", 
-        season: "Winter", 
+        originCountry: "Japan",
+        season: "Winter",
         stockVolumeInKg: 1600,
         saleLocations: ["GL", "GR", "BE", "VS"],
     },
-    { 
+    {
         fishType: "Sailfish",
         price: 5.20,
-        entryDate: new Date(2021, 3, 19),    
+        entryDate: new Date(2021, 3, 19),
         durationInDays: 15,
         itemWeightInGrams: 240,
-        originCountry: "United Kingdom", 
-        season: "Spring", 
+        originCountry: "United Kingdom",
+        season: "Spring",
         stockVolumeInKg: 500,
         saleLocations: ["ZH", "SH", "BL", "SO"],
     },
-    { 
+    {
         fishType: "Red Drum",
         price: 3.10,
-        entryDate: new Date(2021, 5, 15),   
+        entryDate: new Date(2021, 5, 15),
         durationInDays: 18,
         itemWeightInGrams: 300,
-        originCountry: "Poland", 
-        season: "Summer", 
+        originCountry: "Poland",
+        season: "Summer",
         stockVolumeInKg: 15500,
         saleLocations: ["FR", "GE", "NE", "TI"],
     },
-    { 
+    {
         fishType: "Pompano",
         price: 10,
-        entryDate: new Date(2021, 8, 17),    
+        entryDate: new Date(2021, 8, 17),
         durationInDays: 20,
         itemWeightInGrams: 645,
-        originCountry: "France", 
-        season: "Autumn", 
+        originCountry: "France",
+        season: "Autumn",
         stockVolumeInKg: 1500,
         saleLocations: ["ZH", "GE", "BE", "VD"],
     },
-    { 
-        fishType: "Bluefish", 
+    {
+        fishType: "Bluefish",
         price: 13.70,
-        entryDate: new Date(2021, 10, 11),  
+        entryDate: new Date(2021, 10, 11),
         durationInDays: 5,
         itemWeightInGrams: 845,
-        originCountry: "Italy", 
-        season: "Winter", 
+        originCountry: "Italy",
+        season: "Winter",
         stockVolumeInKg: 200,
         saleLocations: ["NW", "OW", "UR", "LU"],
     },
-    { 
+    {
         fishType: "Mackerel",
         price: 8.90,
-        entryDate: new Date(2021, 6, 18),    
+        entryDate: new Date(2021, 6, 18),
         durationInDays: 16,
         itemWeightInGrams: 150,
-        originCountry: "GREECE", 
-        season: "Summer", 
+        originCountry: "GREECE",
+        season: "Summer",
         stockVolumeInKg: 8100,
         saleLocations: ["AG", "BL", "BE", "VD", "TG"],
     },
-    { 
+    {
         fishType: "Perch",
         price: 11.90,
-        entryDate: new Date(2021, 10, 1),  
+        entryDate: new Date(2021, 10, 1),
         durationInDays: 30,
         itemWeightInGrams: 222,
-        originCountry: "Egypt", 
-        season: "Summer", 
+        originCountry: "Egypt",
+        season: "Summer",
         stockVolumeInKg: 9500,
         saleLocations: ["TI", "GE", "ZH", "VD", "AR"],
     },
-    { 
+    {
         fishType: "Mullet",
         price: 4.80,
-        entryDate: new Date(2021, 2, 14),   
+        entryDate: new Date(2021, 2, 14),
         durationInDays: 30,
         itemWeightInGrams: 333,
-        originCountry: "Vietnam", 
-        season: "Summer", 
+        originCountry: "Vietnam",
+        season: "Summer",
         stockVolumeInKg: 600,
         saleLocations: ["ZH", "GE", "BE", "VD"],
     },
-    { 
+    {
         fishType: "Tuna",
         price: 19.50,
-        entryDate: new Date(2021, 0, 1),    
+        entryDate: new Date(2021, 0, 1),
         durationInDays: 30,
         itemWeightInGrams: 250,
-        originCountry: "Spain", 
-        season: "Winter", 
+        originCountry: "Spain",
+        season: "Winter",
         stockVolumeInKg: 2300,
         saleLocations: ["ZH", "VD", "BS", "TI", "SG"],
     }
@@ -139,5 +138,5 @@ const europeanCountry = [
     "Hungary",
     "Sweden",
     "Ireland",
-  ];
-  const swissRomandeRegion = ["FR", "GE", "JU", "NE", "VL", "VD"];
+];
+const swissRomandeRegion = ["FR", "GE", "JU", "NE", "VL", "VD"];
--- function.js
+++ function.js
@@ -1,105 +1,170 @@
-// // // Cabbar, Kerem ve Sinan ortaklasa balik ciftligi projesine girerler. Cabbar ve Keremin referansi ile Sinan da bizlerden bir program istemektedir.
-// // // Uc kafadar bizden ciftlikteki baliklarin satisi icin bir online sistem istemektedirler. Online satis sistemi asagidaki sorulara cevap verebilmelidir.
+//  1) Stok miktari 500 kg uzerinde olan baliklarin isimleri nelerdir?
 
-// // // 1) Stok miktari 500 kg uzerinde olan baliklarin isimleri nelerdir?
-// // // 2) Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
-// // // 3) Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
-// // // 4) Son kullanma tarihlerine gore baliklari siralayiniz. (Son kullanma tarihi yaklasan baliklar once gosterilmelidir)
-// // // 5) Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya gore siralayiniz.
-// // // 6) Toplam balik stoku ne kadardir?
-// // // 7) En pahali olan balik hangisidir?
-// // // 8) En uzun sureli durabilen baliklar hangi ulkeden gelmektedir?
-// // // 9) Kis ve sonbahar sezonu icin swiss romande region'da satilan baliklarin ortalama fiyati nedir?
-// // // 10) TC Kantonu icin stokta toplam ne kadar balik mevcuttur?
-// // // 11) Yazlik sezonda cikan ve AB disindan gelen ve de ZH'de satilan baliklarin ortalama gramajini bulunuz?
+let bul500UzeriOlanlar = [];
 
-// //  1) Stok miktari 500 kg uzerinde olan baliklarin isimleri nelerdir?
-
-// let bul500UzeriOlanlar = [];
-// function bulBaliklari500UzeriOlalari(){
-//     for(let index = 0; index < fishFarm.length; index++){
-//         const element = fishFarm[index].stockVolumeInKg
-//         if (element > 500){
-//             bul500UzeriOlanlar.push(fishFarm[index].fishType)
-//         }
-//     };
-//     return console.log("500 kg uzeri olan baliklar" + bul500UzeriOlanlar )
-// };
-// bulBaliklari500UzeriOlalari()
-
-
-
-//  // 2) Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
-// let fiyatAraligi9Ve12Olan= [];
-// function bulFiyatAraligi9Ve12Olan(){
-//     for(let index = 0; index < fishFarm.length; index++){
-//         const element = fishFarm[index].price
-//         if (element >=9 && element <=12){
-//             fiyatAraligi9Ve12Olan.push(fishFarm[index].fishType)
-//         }
-//     };
-//     return console.log("Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar"  +fiyatAraligi9Ve12Olan )
-// };
-// bulFiyatAraligi9Ve12Olan()
-
-//  // 3) Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
-
-// let kisSezonuVeBerndeBalik=[];
-// function bulKisSezonuVeBerndeBalik(){
-//     for(let index=0; index<fishFarm.length; index++){
-//         const element=fishFarm[index].season
-//         const element1=fishFarm[index].saleLocations
-//         if (season=="Winter" && saleLocations=="BE"){
-//             kisSezonuVeBerndeBalik.push(fishFarm[index].fishType)
-//         }
-//     };
-//     return console.log("Sadece Bern'de ve kis sezonu satilan baliklar" + kisSezonuVeBerndeBalik )
-// };
-// bulKisSezonuVeBerndeBalik()
-
-// // 4) Son kullanma tarihlerine gore baliklari siralayiniz. (Son kullanma tarihi yaklasan baliklar once gosterilmelidir)
-
-// let baliklarinTarihleri=[];
-// function bulBaliklarinTarihleri(){
-//     for(let index=0; index<fishFarm.length; index++){
-//         fishFarm.sort(function(x,y){
-//             return x.entryDate-y.entryDate
-//         });
-//             baliklarinTarihleri.push(fishFarm[index].fishType + " " + fishFarm[index].entryDate + "/n")
-        
-//     };
-//         console.log("sirali"+baliklarinTarihleri)
-        
-//     };
-//     bulBaliklarinTarihleri()
-
-//     // 5) Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya gore siralayiniz. 
-
-
-
-//     // 6) Toplam balik stoku ne kadardir?
-
-//     function toplamStok(){
-//         let toplam=0;
-//         for(let index=0; index<fishFarm.length; index++){
-//             toplam=toplam+fishFarm[index].stockVolumeInKg
-//         };
-//             console.log("Toplam balik stoku"+toplam)
-//     };
-//     toplamStok()
-
-
-    // 7) En pahali olan balik hangisidir?
-
-        function enPahaliBalik(){
-                let fiyat=0
-                let isim;
-                for(let index=0; index<fishFarm.length; index++){
-                    if(fishFarm[index].price>fiyat){
-                        fiyat=fishFarm[index].price
-                        isim=fishFarm[index].fishType
-                    };
-        };
-        console.log("En pahali olan balik"+" "+fiyat+" "+"cinsi"+isim)
+function bulBaliklari500UzeriOlalari() {
+    for (let index = 0; index < fishFarm.length; index++) {
+        const element = fishFarm[index].stockVolumeInKg
+        if (element > 500) {
+            bul500UzeriOlanlar.push(fishFarm[index].fishType)
+        }
     };
-        enPahaliBalik()
+    return console.log("500 kg uzeri olan baliklar" + bul500UzeriOlanlar)
+};
+bulBaliklari500UzeriOlalari()
+
+
+
+// 2) Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar hangileridir?
+let fiyatAraligi9Ve12Olan = [];
+
+function bulFiyatAraligi9Ve12Olan() {
+    for (let index = 0; index < fishFarm.length; index++) {
+        const element = fishFarm[index].price
+        if (element >= 9 && element <= 12) {
+            fiyatAraligi9Ve12Olan.push(fishFarm[index].fishType)
+        }
+    };
+    return console.log("Fiyat araligi 9Fr. ile 12 Fr. arasindaki baliklar" + fiyatAraligi9Ve12Olan)
+};
+bulFiyatAraligi9Ve12Olan()
+
+// 3) Sadece Bern'de ve kis sezonu satilan baliklar hangileridir?
+
+let kisSezonuVeBerndeBalik = [];
+
+function bulKisSezonuVeBerndeBalik() {
+    for (let index = 0; index < fishFarm.length; index++) {
+        const character = fishFarm[index].season
+        const character1 = fishFarm[index].saleLocations
+        if (character == "Winter" && character1 == "BE") {
+            kisSezonuVeBerndeBalik.push(fishFarm[index].fishType + " " + fishFarm[index].season + " " + fishFarm[index].saleLocations)
+
+        }
+    };
+    return console.log("Sadece Bern'de ve kis sezonu satilan baliklar" + kisSezonuVeBerndeBalik)
+};
+bulKisSezonuVeBerndeBalik()
+
+// 4) Son kullanma tarihlerine gore baliklari siralayiniz. (Son kullanma tarihi yaklasan baliklar once gosterilmelidir)
+
+let baliklarinTarihleri = [];
+
+function bulBaliklarinTarihleri() {
+    for (let index = 0; index < fishFarm.length; index++) {
+        fishFarm.sort(function (x, y) {
+            return x.entryDate - y.entryDate
+        });
+        baliklarinTarihleri.push(fishFarm[index].fishType + " " + fishFarm[index].entryDate + "/n")
+
+    };
+    console.log("sirali" + baliklarinTarihleri)
+
+};
+bulBaliklarinTarihleri()
+
+// 5) Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik siraya gore siralayiniz. 
+function ABdenGelen10FrKucukAlfabetik() {
+    let bulABdenGelen10FrKucukAlfabetik = [];
+    for (let index = 0; index < fishFarm.length; index++) {
+        if (europeanCountry.indexOf(fishFarm[index].originCountry) > 0 && fishFarm[index].price > 10)
+            bulABdenGelen10FrKucukAlfabetik.push(fishFarm[index].fishType)
+    };
+    bulABdenGelen10FrKucukAlfabetik.sort();
+    console.log("Avrupa Birligi'nden (AB) gelen ve fiyati 10Fr dan dusuk olan baliklari alfabetik sirasi" + " " + bulABdenGelen10FrKucukAlfabetik)
+
+};
+ABdenGelen10FrKucukAlfabetik()
+
+
+// 6) Toplam balik stoku ne kadardir?
+
+function toplamStok() {
+    let toplam = 0;
+    for (let index = 0; index < fishFarm.length; index++) {
+        toplam = toplam + fishFarm[index].stockVolumeInKg
+    };
+    console.log("Toplam balik stogu :" + " " + toplam)
+};
+toplamStok()
+
+
+// 7) En pahali olan balik hangisidir?
+
+function enPahaliBalik() {
+    let fiyat = 0
+    let isim;
+    for (let index = 0; index < fishFarm.length; index++) {
+        if (fishFarm[index].price > fiyat) {
+            fiyat = fishFarm[index].price
+            isim = fishFarm[index].fishType
+        };
+    };
+    console.log("En pahali olan balik:" + " " + isim + ", " + "fiyat:" + " " + fiyat)
+};
+enPahaliBalik()
+
+// 8) En uzun sureli durabilen baliklar hangi ulkeden gelmektedir?
+
+let EnUzunSureDuranBalik = [];
+
+function bulEnUzunSureDuranBalik() {
+    for (let index = 0; index < fishFarm.length; index++) {
+        const element = fishFarm[index].durationInDays
+        if (element > 25) {
+            EnUzunSureDuranBalik.push(fishFarm[index].fishType + " " + fishFarm[index].durationInDays + " " + fishFarm[index].originCountry)
+        }
+    };
+    return console.log("En uzun sureli durabilen baliklar" + EnUzunSureDuranBalik)
+};
+bulEnUzunSureDuranBalik()
+
+// 9) Kis ve sonbahar sezonu icin swiss romande region'da satilan baliklarin ortalama fiyati nedir?
+function kisVeSonbaharOrtalamaFiyat() {
+    let allRegion = 0;
+    let all = 0;
+    for (let index = 0; index < fishFarm.length; index++) {
+        if (fishFarm[index].season == "Winter" || fishFarm[index].season == "Autumn") {
+            const kesisim = swissRomandeRegion.filter(element => fishFarm[index].saleLocations.includes(element));
+            if (kesisim.length > 0) {
+                all = all + fishFarm[index].price;
+                allRegion++;
+            }
+        }
+    };
+    console.log(Math.floor(all / allRegion))
+
+};
+kisVeSonbaharOrtalamaFiyat()
+
+// 10) TC Kantonu icin stokta toplam ne kadar balik mevcuttur?
+
+function TcKantonuStok() {
+    let all = 0;
+    for (let index = 0; index < fishFarm.length; index++) {
+        if (fishFarm[index].saleLocations.includes("TI")) {
+            all = all + fishFarm[index].stockVolumeInKg;
+        }
+    };
+    console.log(all)
+};
+TcKantonuStok()
+
+// 11) Yazlik sezonda cikan ve AB disindan gelen ve de ZH'de satilan baliklarin ortalama gramajini bulunuz?
+
+function yazlikZHDekiOrtalamaGramaj() {
+    let allRegion = 0;
+    let all = 0;
+
+
+    for (let index = 0; index < fishFarm.length; index++) {
+
+        if (fishFarm[index].season == "Summer" && europeanCountry.indexOf(fishFarm[index].originCountry) == -1 && fishFarm[index].saleLocations.includes("ZH")) {
+            all = all + fishFarm[index].price;
+            allRegion++
+        }
+    };
+    console.log(Math.floor(all / allRegion))
+
+};
+yazlikZHDekiOrtalamaGramaj()
